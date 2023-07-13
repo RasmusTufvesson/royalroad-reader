14b39c2fd6f362876650f84a3170cb62c85003d9
RasmusTufvesson <lars@bizzbook.com>
2023-07-14 01:30:29 +0200

    add getting content from chapters

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod royalroad;
 
 fn main() {
-    let story = royalroad::Story::from_page_url("https://www.royalroad.com/fiction/15935/there-is-no-epic-loot-here-only-puns".to_string());
-    println!("{:?}", story);
+    let mut story = royalroad::Story::from_page_url("https://www.royalroad.com/fiction/15935/there-is-no-epic-loot-here-only-puns".to_string());
+    story.chapters[201].load_content();
+    println!("{:?}", story.chapters[201].content);
 }
--- src/royalroad.rs
+++ src/royalroad.rs
@@ -5,15 +5,38 @@ use regex::Regex;
 
 #[derive(Debug)]
 pub struct ChapterContent {
-    start_note: Option<String>,
-    chapter_content: String,
-    end_note: Option<String>,
+    pub start_note: Option<String>,
+    pub chapter_content: String,
+    pub end_note: Option<String>,
+}
+
+impl ChapterContent {
+    pub fn from_url(url: &str) -> Self {
+        let content = reqwest::blocking::get(url)
+            .expect("Coundnt get page")
+            .text()
+            .expect("Couldnt get text from page");
+        lazy_static! {
+            static ref content_regex: Regex = Regex::new(
+                r#"(?:<div class="portlet solid author-note-portlet">[\t\n ]*<div class="portlet-title">[\t\n ]*<div class="caption">[\t\n ]*<i class="fa fa-sticky-note"><\/i>[\t\n ]*<span class="caption-subject bold uppercase">.*?<\/span>[\t\n ]*<\/div>[\t\n ]*<\/div>[\t\n ]*<div class="portlet-body author-note">(.*?)<\/div>[\t\n ]*<\/div>[\t\n ]*)?<div class="chapter-inner chapter-content">([\s\S]*?)<\/div>(?:[\t\n ]*<h6 class="bold uppercase text-center">Advertisement<\/h6>[\t\n ]*<div class="wide text-center ">[\t\n ]*<div id="Content_Mid">[\t\n ]*<\/div>[\t\n ]*<\/div>[\t\n ]*)?(?:<div class="portlet solid author-note-portlet">[\t\n ]*<div class="portlet-title">[\t\n ]*<div class="caption">[\t\n ]*<i class="fa fa-sticky-note"><\/i>[\t\n ]*<span class="caption-subject bold uppercase">.*?<\/span>[\t\n ]*<\/div>[\t\n ]*<\/div>[\t\n ]*<div class="portlet-body author-note">(.*?)<\/div>[\t\n ]*<\/div>)?"#
+            ).unwrap();
+        }
+        let caps = content_regex.captures(&content).unwrap();
+        let start_note = caps.get(1).and_then(|x| Some(x.as_str().to_string()));
+        let end_note = caps.get(3).and_then(|x| Some(x.as_str().to_string()));
+        let chap_content = caps.get(2).unwrap().as_str();
+        Self {
+            start_note,
+            chapter_content: chap_content.to_string(),
+            end_note,
+        }
+    }
 }
 
 #[derive(Debug)]
 pub struct Chapter {
-    content: Option<ChapterContent>,
-    name: String,
+    pub content: Option<ChapterContent>,
+    pub name: String,
     url: String,
 }
 
@@ -21,20 +44,23 @@ impl Chapter {
     pub fn from_name_and_url(name: String, url: String) -> Self {
         Self { content: None, name, url }
     }
+
+    pub fn load_content(&mut self) {
+        self.content = Some(ChapterContent::from_url(&self.url));
+    }
 }
 
 #[derive(Debug)]
 pub struct Story {
-    title: String,
+    pub title: String,
     id: u32,
-    page_url: String,
-    desctiption: String,
-    chapters: Vec<Chapter>,
+    pub page_url: String,
+    pub desctiption: String,
+    pub chapters: Vec<Chapter>,
 }
 
 impl Story {
     pub fn from_page_url(url: String) -> Self {
-        let url = if url.ends_with("/") { url[..url.len()-1].to_string() } else { url };
         let content = reqwest::blocking::get(&url)
             .expect("Coundnt get page")
             .text()
@@ -53,7 +79,7 @@ impl Story {
         let id = id_regex.captures(&url).unwrap().get(1).unwrap().as_str();
         let description = description_regex.captures(&content).unwrap().get(1).unwrap().as_str();
         let chapters = chapter_regex.captures_iter(&content).map(|x|
-            Chapter::from_name_and_url(x.get(2).unwrap().as_str().to_string(), url.clone() + x.get(1).unwrap().as_str())
+            Chapter::from_name_and_url(x.get(2).unwrap().as_str().to_string(), "https://www.royalroad.com".to_string() + x.get(1).unwrap().as_str())
         ).collect();
         Self {
             title: title.to_string(),
